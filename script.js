308d6df6f958d4fa97f4380013ec7a9aab375614
Trygve-F <trygvefinley@gmail.com>
2022-02-10 19:57:18 -0800

    button clicks now display clear button clears display

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
+//to do next:
+//displayValue replace console log with something that sends this.value to display and keeps it there.
+//function to take displays contents and send to operate function
+//let display = document.getElementsByClassName('display');
+let buttons = document.querySelectorAll('.input');
+    buttons.forEach(buttons => {
+    buttons.addEventListener('click', displayValue);
+    });
+
+let clearButton = document.querySelector('.clear');
+    clearButton.addEventListener('click', clearDisplay);
+
+
+function displayValue() {
+    document.getElementsByClassName('input').textContent = this.value;
+    let display = document.querySelector('#display');
+    display.textContent += this.value
+    return this.value
+};
+
+function clearDisplay(display) {
+    let remove = document.querySelector('#display');
+    remove.textContent = null
+    }
 
 function add (num1,num2) {
     let sum = num1 + num2;
     return sum;
-}
+};
 
 function subtract(num1,num2) {
     let difference = num1 - num2;
     return difference;
-}
+};
 
 function multiply(num1,num2) {
     let product = num1 * num2;
     return product;
-} 
+} ;
 
 function divide(num1,num2) {
     if (num1 === 0 || num2 === 0) {
-        alert('Error, dividing by 0 is hard')
+        alert('Error, dividing by 0 is hard');
     } {
     let quotient = num1/num2;
-    return quotient
+    return quotient;
     }
-}
+};
 
 function operate(operator,num1,num2) {
     switch(true) {
@@ -39,3 +63,4 @@ function operate(operator,num1,num2) {
             break;
     }
 }
+
